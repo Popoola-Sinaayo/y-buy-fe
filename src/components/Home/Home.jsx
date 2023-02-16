f3780e4bc9c5900ff69fec7bf3d3131a896a876f
Popoola-Sinaayo <olusegunpopoola4real@gmail.com>
2023-02-16 20:42:16 +0100

    initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div>
+      <div className="home-header">
+        <div className="home-title"></div>
+        <div className="home-navigation"></div>
+        <div className="home-icons"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
